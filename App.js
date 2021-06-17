d92ee033b6987c904327dbb95d0fad9e5f3ed612
Jacopo Rodighiero <jacoporodighiero@Jacopos-MacBook-Pro-2.local>
2021-06-17 13:49:27 +0200

    navigation implemented

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import IndexScreen from "./src/screens/IndexScreen";
+import * as React from "react";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your pup!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{ gestureEnabled: false }}
+        >
+          <Stack.Screen
+            name="Home"
+            component={IndexScreen}
+            options={{ title: "My app" }}
+          />
+        </Stack.Navigator>
+      }
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const IndexScreen = () => {
+
+    return(
+        <View>
+            <Text>Hola World</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
